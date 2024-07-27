a03a686b5a6e9cf6159c9fcbbe9ed82c13f877ef
June Bee <junebee66@gmail.com>
2024-07-27 18:58:41 -0400

    upload menu code

--- menu/menu.js
+++ menu/menu.js
@@ -1,4 +1,4 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("appetizer-section");
 var i;
 
 for (i = 0; i < coll.length; i++) {
@@ -12,3 +12,96 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+var pro = document.getElementsByClassName("produce-section");
+var a;
+
+for (a = 0; a < pro.length; a++) {
+  pro[a].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var ent = document.getElementsByClassName("entree-section");
+var e;
+
+for (e = 0; e < pro.length; e++) {
+  ent[e].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var sid = document.getElementsByClassName("sides-section");
+var s;
+
+for (s = 0; s < pro.length; s++) {
+  sid[s].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var des = document.getElementsByClassName("desserts-section");
+var d;
+
+for (d = 0; d < pro.length; d++) {
+  des[d].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var ref = document.getElementsByClassName("drinks-section");
+var r;
+
+for (r = 0; r < pro.length; r++) {
+  ref[r].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+var coc = document.getElementsByClassName("cocktails-section");
+var c;
+
+for (c = 0; c < pro.length; c++) {
+  coc[c].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
